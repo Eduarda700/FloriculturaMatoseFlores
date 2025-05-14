5a9d9d3352b60420d5fed5648395aff69735c9aa
Eduarda700 <Lopesdudah037@gmail.com>
2025-05-14 20:33:43 -0300

    Add files via upload

---
+++ teste1/usuarioCliente.php
@@ -0,0 +1,17 @@
+<?php
+class usuario {
+    public $codigo, $nome, $email, $senha, $telefone;
+
+    function __construct($codigo, $nome, $email, $senha){
+        $this->codigo = $codigo;
+        $this->nome = $nome;
+        $this->senha = $senha;
+        $this->email = $email;
+        $this->telefone = $telefone;
+       
+    }
+
+    function validaUsuarioSenha($email, $senha){
+        return $email === $this->email && $senha === $this->senha;
+    }
+}
